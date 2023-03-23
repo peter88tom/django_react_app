cd91c55ebd631811a527f184fbb1ea0b2d337ed9
petro_thomas <peter88tom@gmail.com>
2023-03-23 10:31:29 +0200

    Add TodoViewset

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
 # Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    """
+    The viewsets base class provides the implementation for CRUD operations by default
+    """
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
